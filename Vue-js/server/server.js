66da9816b562ca0cb59e0288aef1bd515ea6c8ba
saranzafar <saranzafar736@gmail.com>
2024-10-18 11:34:58 -0700

    fix bugs at server

--- Vue-js/server/server.js
+++ Vue-js/server/server.js
@@ -1,13 +1,10 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('jobs.json');
+const router = jsonServer.router(path.join(__dirname, 'jobs.json'));
 const middlewares = jsonServer.defaults();
 
 
 server.use(middlewares);
 server.use(router);
 
-
-module.exports = (req, res) => {
-    server(req, res);
-};
+module.exports = server;
