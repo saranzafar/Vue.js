971804c3f27e0e621f3e38dbf121ee6b3551f4a7
saranzafar <saranzafar736@gmail.com>
2024-10-18 11:48:16 -0700

    add path to server file

--- Vue-js/server/server.js
+++ Vue-js/server/server.js
@@ -1,8 +1,8 @@
+const path = require('path');
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, 'jobs.json'));
 const middlewares = jsonServer.defaults();
-
 
 server.use(middlewares);
 server.use(router);
