44cadc6bdd12eba1f0209f512ae80ac8e84e5931
saranzafar <saranzafar736@gmail.com>
2024-10-18 21:17:02 -0700

    updated vercel.json

--- Vue-js/client/vite.config.js
+++ Vue-js/client/vite.config.js
@@ -3,9 +3,11 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 // Define backend URL based on environment
-const apiUrl = process.env.NODE_ENV === 'production'
-    ? 'https://vue-js-server-hazel.vercel.app'
-    : 'http://localhost:5000'; 
+// const apiUrl = process.env.NODE_ENV === 'production'
+//     ? 'https://vue-js-server-hazel.vercel.app'
+//     : 'http://localhost:5000'; 
+
+const apiUrl = 'https://vue-js-server-hazel.vercel.app';
 
 export default defineConfig({
     plugins: [vue()],
